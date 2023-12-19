5c197ece2e232eca73d262ef5587e2cdf22efd29
Marteen Motun MUbaraq <motunmarteen@gmail.com>
2023-12-19 19:09:23 +0100

    variable declaration and initialization using single line and separate line for declaration and initialization

---
+++ Day2/script.js
@@ -0,0 +1,20 @@
+// let is used in mordern javascript to declare variables
+
+let message;
+message = "hello";
+alert(message); // shows the variable content
+
+// Single Line Variable: The Let keyword is used for all the 3 variable initializations
+let user = "John", age = 25, message = "Hello";
+
+// Separate Line:
+let user = "John";
+let age = 25;
+let message = "Hello";
+
+// Note that "let" keyword is used more often than the "var" keyword
+// The "let" keyword is the modern way of writing Javascript while
+// the "var" keyword is the old way of writing Javascript
+
+// Note that the variable is like a box that is saving information
+
