5ee8cba233c2cb6950a5dbebc1108e54e46d761c
Marteen Motun MUbaraq <motunmarteen@gmail.com>
2023-12-19 20:06:40 +0100

    Datatypes in javascript

---
+++ Day2/datatypes.js
@@ -0,0 +1,53 @@
+// Number
+let n = 344
+n = 3.1415
+// Note that "INFINITY" occurs when trying to divide a number by 0
+alert(39580205/0)
+// Note that "-INFINITY" occurs when trying to divide a negative number by 0
+alert(-39580205/0)
+// Note that "NaN" meaning "Not a Number" occurs when trying to divide a string by 0
+alert("string"/0)
+
+
+// BigInt
+// When working with large number, we have to add "n" at the end to make it valuable and come out "AS IT IS"
+alert(643987739378479897787921801892538927643782647872894278642982625827762829616515368197426428649164683968454286498458457427239732862375723638263265823696372359136573169133617n)
+// (2^53-1)
+// -(2^53-1)
+
+
+// String
+let age = 50;
+let string1 = "I am 20 years old"
+// There is no difference between single quotation and double quotation
+alert(string1)
+// there is another quotation mark called backtick quotation mark "`"
+let string2 = `I am ${age} years old`
+// the ${} is the interpolation that helps to invite variable
+// Backtick is needed to invite variables in javascript
+alert(string2)
+
+
+// Boolean
+let checked = false
+alert(checked)
+let checked2 = true
+alert(checked2)
+
+// Null Value
+let null1 = null;
+alert(null1)
+
+// Undefined Value
+let age2
+alert(age2)
+// Note that the value of age2 will be undefined because there is no value
+// given to the variable.
+
+
+// Objects
+// They are used to store collection of data in complex entities.
+
+
+//  Symbols
+// They are used to create unique identifiers for objects
