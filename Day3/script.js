41bc9aa50895c3c66a9f28c510ad2f28563dd3d8
Marteen Motun MUbaraq <motunmarteen@gmail.com>
2023-12-20 22:16:21 +0100

    using typeof function to check the data type of a value

---
+++ Day3/script.js
@@ -0,0 +1,30 @@
+"use strict"
+
+// typeof is a function used to know the type of a data a value is associated with
+
+alert(typeof(true))
+// This alerts boolean
+
+alert(typeof(343))
+// This alerts number
+
+alert(typeof(34342972974n))
+// This alerts bigint
+
+alert(typeof("ade is a boy"))
+// This alerts string
+
+let name;
+alert(typeof(name))
+// This alerts undefined
+
+alert(typeof(Math))
+// This alerts object
+
+alert(typeof("31686318"/3))
+// This alerts Nan (Not a Number)
+
+
+
+
+
