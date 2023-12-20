7fa5147ca2b5e462d3b60c2f47996021de1c699e
Marteen Motun MUbaraq <motunmarteen@gmail.com>
2023-12-20 23:10:17 +0100

    Basic math operators

---
+++ Day3/mathoperators.js
@@ -0,0 +1,41 @@
+// Basic Math Operators
+
+// false means 0 or no
+
+alert(21 > 23)
+// This will alert false because 21 is not greater than 23
+
+alert(21 < 23)
+// This will alert true because 21 is less than 23
+
+alert(21 == 23)
+// This will alert false because 21 is not equal to 23
+
+alert(21 != 23)
+// This will alert true because 21 is not equal to 23
+
+// This can also be used for strings. Eg below:
+alert("Z" > "A")
+// This will alert true because Z is bigger than A
+
+alert("Glow" > "Glee")
+// This will alert true because Glow comes later than Glee in the dictionary
+
+alert("Apple" > "Glee")
+// This will alert false because Apple comes before Glee in the dictionary
+
+alert("2" > 1)
+// This will alert true because Javascript has a smart function that will help us check if the string has a number in it first. If yes, the number will then be used to compare the other number to know if it is true or false.
+
+
+alert(true == 1)
+// true means 1 or yes so therefore, this will return true
+
+alert(false == 0)
+// false means 0 or no so therefore, this will return true
+
+// Note that if we use the "strict equality sign (===)", the response will be false because javascript will strictly check the compared data to be strictly the same not just when they mean the same.
+
+
+
+
