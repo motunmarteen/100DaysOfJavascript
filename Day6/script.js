1b15ff4113e040cd5c24b25db5755f01bf7f174a
Marteen Motun MUbaraq <motunmarteen@gmail.com>
2023-12-23 22:52:13 +0100

    Logical operator, using the OR, AND and NOT operators for logical Programming

--- Day6/script.js
+++ Day6/script.js
@@ -1,2 +1,54 @@
-alert("100 Days of JavaScript - Day 6")
+// alert("100 Days of JavaScript - Day 6")
 // console.log("100 Days of JavaScript - Day 6")
+
+/* Logical Operators
+OR ...............................  (||) It has same precedence as AND
+AND ............................... (&&) It has same precedence as OR
+NOT ............................... (!) This has the highest Precedence
+NULLISH COERCING .................. (??) This is not used often
+*/
+
+/* OR OPERATOR
+true || true => true
+true || false => true
+false || true => true
+false || false => false
+*/
+
+/* AND OPERATOR
+true && true => true
+true && false => false
+false && true => false
+false && false => false
+NOTE: in AND operator, if any object is false it bring a false response
+*/
+
+/* NOT OPERATOR
+    !true => false
+    !false => true
+*/
+
+a = 12;
+b = 23;
+c = 56;
+
+if ((a == 12) || (b == 23) || (c == 56)) {
+    alert("True Returned")
+} 
+else {
+    alert("False Returned")
+}
+
+// Lets change the b to 28 in the if statement
+if ((a == 12) && (b == 28) && (c == 56)) {
+    alert("True Returned")
+} 
+else {
+    alert("False Returned")
+}
+
+a = true;
+x = false;
+
+alert(!a)
+alert(!x)
