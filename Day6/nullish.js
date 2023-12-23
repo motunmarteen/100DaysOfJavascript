315114ef4e53f201d16d2de1ad3e7c678770232a
Marteen Motun MUbaraq <motunmarteen@gmail.com>
2023-12-23 23:10:30 +0100

    Nullish coalescing operator

---
+++ Day6/nullish.js
@@ -0,0 +1,12 @@
+// Nullish Coalescing operator
+
+// ??
+
+// a ?? b => if a id defined, then a else b
+
+let firstName = null
+let lastName = null
+// let nickName = "Supercoder"
+let nickName = null
+
+alert(firstName ?? lastName ?? nickName ?? "Anonymous")
