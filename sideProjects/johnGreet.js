7a0f6cca729b78cbe2346d93e3dc09add2a590fe
Marteen Motun MUbaraq <motunmarteen@gmail.com>
2023-12-24 17:59:27 +0100

    Write a program that prints 'Hello if a user's name is 'John, otherwise prints 'Greetings'.

---
+++ sideProjects/johnGreet.js
@@ -0,0 +1,9 @@
+//  Write a program that prints "Hello" if a user's name is "John", otherwise prints "Greetings".
+
+let userName = prompt("What is your name?")
+
+if (userName == "John") {
+    alert("Hello")
+} else {
+    alert("Greetings")
+}
