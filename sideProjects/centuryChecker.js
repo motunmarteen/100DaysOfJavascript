3d1d2acd8807abbd21dfbeb4c0b04108f4e060d7
Marteen Motun MUbaraq <motunmarteen@gmail.com>
2023-12-24 17:48:23 +0100

    Create a program that checks if a year is within the 21st century.

---
+++ sideProjects/centuryChecker.js
@@ -0,0 +1,9 @@
+//  Create a program that checks if a year is within the 21st century.
+
+let year = prompt("What year do you want to confirm if it is 21st Century?")
+
+if ((year >= 2001) && (year <= 2100)) {
+    alert("YES!!! THIS IS A 21st CENTURY YEAR")
+} else {
+    alert("NO!!! THIS IS NOT A 21st CENTURY YEAR")
+}
