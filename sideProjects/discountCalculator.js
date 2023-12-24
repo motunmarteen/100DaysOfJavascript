40358358ef7ce9b970173976f5283a5f021d6ba9
Marteen Motun MUbaraq <motunmarteen@gmail.com>
2023-12-24 16:44:01 +0100

    Create a program that calculates the discount percentage based on the purchase amount.

--- sideProjects/discountCalculator.js
+++ sideProjects/discountCalculator.js
@@ -4,4 +4,4 @@ let purchaseAmount = Number(prompt("How much is the total cost of good sold and 
 
 let discount = (discountPercentage / 100) * purchaseAmount
 let finalCost = purchaseAmount - discount
-alert(`The total cost of your goods and service is ${purchaseAmount} and you have been given a discount of ${discountPercentage}% which is $${discount}. Now, you are to pay are total cost of $${finalCost}.`)
+alert(`The total cost of your goods and service is $${purchaseAmount} and you have been given a discount of ${discountPercentage}% which is $${discount}. Now, you are to pay are total cost of $${finalCost}.`)
