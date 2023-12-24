6fa23146080feac5cf853c060908da604d124642
Marteen Motun MUbaraq <motunmarteen@gmail.com>
2023-12-24 15:15:38 +0100

    if (a string with zero)
    Will alert be shown?

---
+++ Day8/assignment2.js
@@ -0,0 +1,17 @@
+/*
+if (a string with zero)
+
+Will alert be shown?
+
+if ("0") {
+
+  alert( 'Hello' );
+
+}
+*/
+
+if ("0") {
+    alert("Hello")
+}
+
+// Yes, "Hello" Will be shown in its alert.
