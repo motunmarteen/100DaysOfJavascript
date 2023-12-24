3d539133ac445454669232a2ebe9e607a6986e81
Marteen Motun MUbaraq <motunmarteen@gmail.com>
2023-12-24 16:16:37 +0100

    Write a program that determines if a user is a teenager (between 13 and 19 years old).

---
+++ sideProjects/checkTeenager.js
@@ -0,0 +1,9 @@
+//  Write a program that determines if a user is a teenager (between 13 and 19 years old).
+
+let age = Number(prompt("What age do you want to check?"))
+
+if ((age >= 13) && (age <= 19)) {
+    alert("You are a Teenager")
+} else {
+    alert("You are not a teenager")
+}
