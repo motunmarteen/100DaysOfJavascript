a72fef5ece4369159e994933abd17e084d9a22c8
Marteen Motun MUbaraq <motunmarteen@gmail.com>
2023-12-24 15:36:14 +0100

    Write a program  that checks if a user is eligible to vote (age 18 and above).

---
+++ sideProjects/voteEligible.js
@@ -0,0 +1,9 @@
+// Write a program  that checks if a user is eligible to vote (age 18 and above).
+
+let votersAge = Number(prompt("How old are you?"));
+
+if (votersAge >= 18) {
+    alert("You are Eligible to vote")
+} else {
+    alert("You are not Eligible to vote")
+}
