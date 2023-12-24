cbec589e9fadbf1d09326138bbde67de326e0698
Marteen Motun MUbaraq <motunmarteen@gmail.com>
2023-12-24 16:23:02 +0100

    write a program that checks if a number is even or odd number

---
+++ sideProjects/evenOrOdd.js
@@ -0,0 +1,9 @@
+// Implement a program that checks if a number is even or odd.
+
+let oddEven = Number(prompt("provide a number to check", 12));
+
+if (oddEven % 2 == 0) {
+    alert("This is an even number")
+} else {
+    alert("This is an odd number")
+}
