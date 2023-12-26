e86715be4f0e7621cc7cc13772cfb3dfc9da0e93
Marteen Motun MUbaraq <motunmarteen@gmail.com>
2023-12-26 15:11:56 +0100

    Passing values to functions... This is done when calling the function

---
+++ Day13/script1.js
@@ -0,0 +1,30 @@
+// Function and Calling in Javascript for Addition
+
+function addition(a, b) {
+    alert(a + b)
+}
+
+addition(6, 8)
+addition(3, 9)
+addition(16, 28)
+
+
+// Function and Calling in Javascript for Subtraction
+
+function subtraction(x, y) {
+    alert(x - y)
+}
+
+subtraction(12, 7)
+subtraction(2, 17)
+subtraction(122, 77)
+
+
+// Function and Calling in Javascript for Expression
+
+function expression(a, b, c, d) {
+    alert(a + b * c - d / a)
+} 
+expression(12, 3, 72, 9)
+expression(11, 33, 72, 19)
+expression(11, 30, 2, 90)
