e80400ab1f9b5417e60cc9da4086e2f77965f84d
Marteen Motun MUbaraq <motunmarteen@gmail.com>
2023-12-26 14:44:55 +0100

    Assignment 5 done and dusted

---
+++ Day11/script4.js
@@ -0,0 +1,10 @@
+//  ASSIGNMENT 4
+
+// Use the for loop to output even numbers from 2 to 10.
+
+
+// SOLUTION 4
+
+for (let num = 2; num <= 10; num += 2) {
+    alert(num);
+}
---
+++ Day11/script5.js
@@ -0,0 +1,16 @@
+// ASSIGNMENT 5
+
+// Rewrite the code changing the for loop to while without altering its behavior (the output should stay same).
+// for (let i = 0; i < 3; i++) {
+//   alert( `number ${i}!` );
+// }
+
+
+// SOLUTION 5
+
+let i = 0;
+
+while (i < 3) {
+    alert(`number ${i}!`);
+    i++
+}
