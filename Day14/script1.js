178e0811aa49ad7e0a7ad36285fa8f1a14ff7b35
Marteen Motun MUbaraq <motunmarteen@gmail.com>
2023-12-27 21:44:29 +0100

    basic mechanism for asking questionvia confirm dialog bog in a browser

--- Day14/script1.js
+++ Day14/script1.js
@@ -1,3 +1,16 @@
+/* 
+This code creates a basic mechanism for asking a user a question via a confirmation dialog box in a browser. The ask function takes three arguments:
+
+question: The message displayed in the confirmation dialog.
+yes: A function to be executed if the user answers "Yes" or agrees.
+no: A function to be executed if the user answers "No" or cancels.
+Then, it defines two functions:
+
+showOk(): Displays an alert saying "You have agreed".
+showCancel(): Displays an alert saying "You canceled the execution".
+Finally, the ask function is called with the message "Do you agree?", and it prompts the user with the confirmation dialog. If the user confirms by clicking "OK", it executes showOk(), showing the "You have agreed" alert. If the user cancels by clicking "Cancel", it executes showCancel(), showing the "You canceled the execution" alert.
+*/
+
 function ask(question, yes, no) {
     if (confirm(question)) {
         yes()
