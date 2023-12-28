7241618612b7cc9a8d973653921207eaa238ccf6
Marteen Motun MUbaraq <motunmarteen@gmail.com>
2023-12-28 12:01:56 +0100

    Objects in javascript

--- Day15/script.js
+++ Day15/script.js
@@ -2,13 +2,21 @@ let family = {
     father : "Motun",
     mother : "Yetunde",
     daughter: "Ateeyah",
-    son : "Azfar"
+    son : "Azfar",
+    another : "noname",
+    "like foods" : true
 }
 
 alert(family.son)
 alert(family.daughter)
 alert(family.father)
 alert(family.mother)
+alert(family.another)
 
-family.admin = true
+family.admin = true // Admin can be added this way
 alert(family.admin)
+
+delete (family.another) // how to delete things in object
+alert(family.another)
+
+alert(family["like foods"]) // This is how to access multi words key in a javascript object
