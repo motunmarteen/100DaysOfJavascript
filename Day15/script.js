817d60c121c2b878f0c3757751b1c253704c9aa4
Marteen Motun MUbaraq <motunmarteen@gmail.com>
2023-12-28 11:55:07 +0100

    Learning Objects in JAvascript

---
+++ Day15/script.js
@@ -0,0 +1,14 @@
+let family = {
+    father : "Motun",
+    mother : "Yetunde",
+    daughter: "Ateeyah",
+    son : "Azfar"
+}
+
+alert(family.son)
+alert(family.daughter)
+alert(family.father)
+alert(family.mother)
+
+family.admin = true
+alert(family.admin)
