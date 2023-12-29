a692380a0ed967ee896f02eda131e5441837be0a
Marteen Motun MUbaraq <motunmarteen@gmail.com>
2023-12-29 13:00:34 +0100

    ipdated

---
+++ Day15/script2.js
@@ -0,0 +1,11 @@
+function user(name, age, foodie) {
+    return {
+        name : name,
+        age : age,
+        foodie :  true
+    }
+}
+let userInfo = user("Motun", 30);
+alert(userInfo.name)
+alert(userInfo.age)
+alert(userInfo.foodie)
