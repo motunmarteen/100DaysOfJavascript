e3f38e5774cfd2ca1ea29118fded5866191b118e
Marteen Motun MUbaraq <motunmarteen@gmail.com>
2023-12-30 14:10:20 +0100

    created a new folder called create element to house the create elements files

--- DOM/script.js
+++
@@ -1,8 +0,0 @@
-let newElement = document.createElement("div");
-console.log(newElement);
-
-let newElement2 = document.createElement("input");
-document.body.appendChild(newElement2)
-
-let newElement3 = document.createElement("h1");
-document.body.appendChild(newElement3)
