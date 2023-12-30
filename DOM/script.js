f67dc4da308ded4fef44a36131f2e5ca75eaec56
Marteen Motun MUbaraq <motunmarteen@gmail.com>
2023-12-30 14:07:18 +0100

    using createElement to create new elements in the Document Object Model for manipulations

---
+++ DOM/script.js
@@ -0,0 +1,8 @@
+let newElement = document.createElement("div");
+console.log(newElement);
+
+let newElement2 = document.createElement("input");
+document.body.appendChild(newElement2)
+
+let newElement3 = document.createElement("h1");
+document.body.appendChild(newElement3)
